5bdf49cbb198c26cb5ac2a132e7a169d90d04466
Dan Canqui <dan.canqui@jalasoft.com>
2016-02-04 13:48:34 -0400

    Implement a basic logic to create an ASTScanner

--- apex-squid/src/main/java/org/fundacionjala/enforce/sonarqube/apex/ApexAstScanner.java
+++ apex-squid/src/main/java/org/fundacionjala/enforce/sonarqube/apex/ApexAstScanner.java
@@ -23,11 +23,19 @@
  */
 package org.fundacionjala.enforce.sonarqube.apex;
 
-import com.sonar.sslr.api.Grammar;
 import java.io.File;
+import java.util.Collection;
+import com.google.common.base.Charsets;
+import com.sonar.sslr.api.Grammar;
+import com.sonar.sslr.impl.Parser;
+import org.fundacionjala.enforce.sonarqube.apex.api.ApexMetric;
 import org.sonar.squidbridge.AstScanner;
 import org.sonar.squidbridge.SquidAstVisitor;
+import org.sonar.squidbridge.SquidAstVisitorContextImpl;
+import org.sonar.squidbridge.api.SourceCode;
 import org.sonar.squidbridge.api.SourceFile;
+import org.sonar.squidbridge.api.SourceProject;
+import org.sonar.squidbridge.indexer.QueryByType;
 
 /**
  * Utility class for scans a file and generate {@link SourceFile}
@@ -48,16 +56,41 @@ public class ApexAstScanner {
      * @return a scanner.
      */
     public static AstScanner<Grammar> create(ApexConfiguration conf, SquidAstVisitor<Grammar>... visitors) {
-        return null;
+        final SourceProject sourceProject = new SourceProject("Apex Project");
+        final SquidAstVisitorContextImpl<Grammar> context = new SquidAstVisitorContextImpl<>(sourceProject);
+        final Parser<Grammar> parser = ApexParser.create(conf);
+
+        AstScanner.Builder<Grammar> builder = AstScanner.<Grammar>builder(context).setBaseParser(parser);
+
+        builder.withMetrics(ApexMetric.values());
+
+        builder.setFilesMetric(ApexMetric.FILES);
+
+        for (SquidAstVisitor<Grammar> visitor : visitors) {
+            builder.withSquidAstVisitor(visitor);
+        }
+
+        return builder.build();
     }
 
     /**
      * Returns a source file from file and visitors.
-     * @param file
-     * @param visitors
+     *
+     * @param file source file.
+     * @param visitors list of visitors.
      * @return a source.
      */
     public static SourceFile scanFile(File file, SquidAstVisitor<Grammar>... visitors) {
-        return null;
+        if (!file.isFile()) {
+            throw new IllegalArgumentException("File '" + file + "' not found.");
+        }
+        AstScanner<Grammar> scanner = create(new ApexConfiguration(Charsets.UTF_8), visitors);
+        scanner.scanFile(file);
+        Collection<SourceCode> sources = scanner.getIndex().search(new QueryByType(SourceFile.class));
+        if (sources.size() != 1) {
+            throw new IllegalStateException("Only one SourceFile was expected whereas " + sources.size() + " has been returned.");
+        }
+        return (SourceFile) sources.iterator().next();
     }
+
 }
