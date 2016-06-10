3e89600ce2d4fa2bc09279ca3204a038a7db4062
Vicente Rodriguez <Vicente.Rodriguez@Jalasoft.com>
2016-06-10 14:39:31 -0400

    Fixed observation about negative rules in ClassOrInterfaceMemberTest

--- apex-squid/src/test/java/org/fundacionjala/enforce/sonarqube/apex/parser/grammar/ApexGrammarClassOrInterfaceMemberTest.java
+++ apex-squid/src/test/java/org/fundacionjala/enforce/sonarqube/apex/parser/grammar/ApexGrammarClassOrInterfaceMemberTest.java
@@ -44,26 +44,15 @@ public class ApexGrammarClassOrInterfaceMemberTest extends ApexRuleTest {
     }
     
     @Test
-    public void positiveRecoveredRules() {
-//        assertThat(parser)
-//                //can be an initializer declaration (only static modifier allowed)
-//                .matches("static {integer wrong a;}");
-//                .matches("{integer x=3; integer y;}") 
-//                //all the other kinds of member can have the other type of modifiers, and they can be:
-//                //a class or an interface declaration
-//                .matches("public without sharing class ClassName {}")
-//                .matches("private with sharing interface iClassName {}")
-//                //an enum declaration
-//                .matches("public enum EnumName{A,B}")
-//                //a constructor
-//                .matches("protected ClassName() {}")
-//                //a field
-//                .matches("private static integer field=3;")
-//                //a method
-//                .matches("public static void doSomething(integer p1);")
-//                //a property
-//                .matches("private string Prop {private get; set;}")
-//                .matches("@someAnnotation\n"
-//                        + "public global static void aMethodWithLoops(list<integer> collection) {}");
+    public void negativeRules() {
+        assertThat(parser)
+                .notMatches("public without sharing without class ClassName {}")
+                .notMatches("private with sharing interface iClassName name {}")
+                .notMatches("publics enum EnumName{A,B}")
+                .notMatches("private static integer field=3%;")
+                .notMatches("public static void doSomething(integerp1);")
+                .notMatches("private string Pr op {private get; set;}")
+                .notMatches("@someAnnotation\n"
+                        + "public global static void aMethodWithLoops(list<<integer> collection) {}");
     }
 }
