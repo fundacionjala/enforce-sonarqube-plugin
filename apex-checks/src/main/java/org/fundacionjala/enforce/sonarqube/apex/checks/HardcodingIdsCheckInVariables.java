3b814772196cc3ceba740b9d9dd8b90c8ea8c726
Kevin Titichoca <kevin.titichoca@jalasoft.com>
2016-07-01 18:44:33 -0400

    fixed hard coded ids check

--- apex-checks/src/main/java/org/fundacionjala/enforce/sonarqube/apex/checks/HardcodingIdsCheckInVariables.java
+++ apex-checks/src/main/java/org/fundacionjala/enforce/sonarqube/apex/checks/HardcodingIdsCheckInVariables.java
@@ -4,16 +4,30 @@
  */
 package org.fundacionjala.enforce.sonarqube.apex.checks;
 
-import com.sonar.sslr.api.AstNode;
-import com.sonar.sslr.api.Grammar;
-import org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.sonar.check.Rule;
 import org.sonar.squidbridge.checks.SquidCheck;
+import org.sonar.sslr.ast.AstSelect;
 
-import java.util.List;
+import com.sonar.sslr.api.AstNode;
+import com.sonar.sslr.api.AstNodeType;
+import com.sonar.sslr.api.Grammar;
 
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.CLASS_OR_INTERFACE_MEMBER;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.EXPRESSION;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.FIELD_DECLARATION;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.LOCAL_VARIABLE_DECLARATION;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.METHOD_DECLARATION;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.PRIMARY_EXPRESSION;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.STATEMENT_EXPRESSION;
 import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.TYPE;
+import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.VARIABLE_DECLARATOR;
 import static org.fundacionjala.enforce.sonarqube.apex.api.grammar.ApexGrammarRuleKey.VARIABLE_INITIALIZER;
+
+import static com.sonar.sslr.api.GenericTokenType.IDENTIFIER;
 
 @Rule(key = HardcodingIdsCheckInVariables.CHECK_KEY)
 public class HardcodingIdsCheckInVariables extends SquidCheck<Grammar> {
@@ -26,52 +40,94 @@ public class HardcodingIdsCheckInVariables extends SquidCheck<Grammar> {
     /**
      * Regular expression of the pattern that matches an apex id.
      */
-    public static final String ID_PATTERN = "(')[a-zA-Z0-9]{15,18}(')";
+    public static final String ID_PATTERN = "(')([a-zA-Z0-9]{18}|[a-zA-Z0-9]{15})(')";
 
     /**
      * Stores a message template.
      */
     private final String MESSAGE = ChecksBundle.getStringFromBundle("IdsInVariablesCheckMessage");
 
+    private Map<String, AstNode> hardcodedVariables;
+
     @Override
     public void init() {
-        subscribeTo(ApexGrammarRuleKey.VARIABLE_DECLARATOR);
+        subscribeTo(METHOD_DECLARATION);
     }
 
     @Override
     public void visitNode(AstNode astNode) {
+        hardcodedVariables = new HashMap<>();
         try {
-        AstNode astParent = astNode.getParent();
-        List<AstNode> typeChildren = astParent.getChildren(TYPE);
-        typeChildren.stream()
-                .filter((currentNode)
-                        -> (currentNode.getTokenOriginalValue()
-                        .matches("ID|id|Id|iD")))
-                .forEach((_item) -> {
-                    checkValue(astNode.getChildren(VARIABLE_INITIALIZER));
-                    _item.getName();
-                });
+            lookForIdVariableHardCoded(astNode);
+            hardcodedVariables.keySet().stream().forEach((hardCodedVariableName) -> {
+                getContext().createLineViolation(this, String.format(MESSAGE,
+                        hardCodedVariableName), hardcodedVariables.get(hardCodedVariableName));
+            });
         } catch (Exception e) {
             ChecksLogger.logCheckError(this.toString(), "visitNode", e.toString());
         }
     }
 
     /**
-     * Checks if one of the given nodes values from the list matches the RegEx
-     * for ID's.
+     * Retrieves all hard-coded values from method and then verifies if these
+     * values belongs to an ID type variable.
      *
-     * @param astNodes the list of nodes to be analyzed.
+     * @param methodDeclarationNode the node where check vists syntax tree.
      */
-    private void checkValue(List<AstNode> astNodes) {
-        astNodes.stream().forEach((current) -> {
-            String astNodeValue = current.getTokenOriginalValue();
-            if (astNodeValue.matches(ID_PATTERN)) {
-                AstNode fieldDeclaration = current.getParent()
-                        .getFirstDescendant(ApexGrammarRuleKey.ALLOWED_KEYWORDS_AS_IDENTIFIER);
-                getContext().createLineViolation(this, String.format(MESSAGE,
-                        fieldDeclaration.getTokenOriginalValue()), fieldDeclaration);
-            }
-        });
+    void lookForIdVariableHardCoded(AstNode methodDeclarationNode) {
+        AstNode classOrInterfaceBodyNode = methodDeclarationNode
+                .getParent().getParent();
+        AstSelect hardCodedExpressions = methodDeclarationNode.select()
+                .descendants(STATEMENT_EXPRESSION).descendants(EXPRESSION);
+        AstSelect localVariableInitializers = methodDeclarationNode.select()
+                .descendants(VARIABLE_INITIALIZER);
+        AstSelect fieldVariableInitializers = classOrInterfaceBodyNode.select().children(CLASS_OR_INTERFACE_MEMBER)
+                .children(FIELD_DECLARATION).children(VARIABLE_DECLARATOR).children(VARIABLE_INITIALIZER);
+        registerHardcodedVariables(hardCodedExpressions, FIELD_DECLARATION, PRIMARY_EXPRESSION, classOrInterfaceBodyNode);
+        registerHardcodedVariables(localVariableInitializers, LOCAL_VARIABLE_DECLARATION, IDENTIFIER, classOrInterfaceBodyNode);
+        registerHardcodedVariables(fieldVariableInitializers, FIELD_DECLARATION, IDENTIFIER, classOrInterfaceBodyNode);
     }
 
+    /**
+     * Given the list of hard-coded values, analyzes them if they match with the
+     * SFDC ID regular expression and if this value belongs to an ID type
+     * variable if so then registers this node into a list of hard-coded
+     * variables detected.
+     *
+     * @param variableInitializers list of hard-coded values found.
+     * @param variableDeclarationType the node type of variable declarations,
+     * according the situation defines what nodes to look for
+     * @param variableValueNodeType the node type of variable values, according
+     * the situation defines what nodes to look for
+     * @param classOrInterfaceBodyNode a superior node where some nodes are
+     * visited from.
+     */
+    void registerHardcodedVariables(AstSelect variableInitializers, AstNodeType variableDeclarationType,
+            AstNodeType variableValueNodeType, AstNode classOrInterfaceBodyNode) {
+        for (AstNode current : variableInitializers) {
+            String variableName = current.getParent()
+                    .getFirstDescendant(variableValueNodeType).getTokenOriginalValue();
+            if (current.getTokenOriginalValue().matches(ID_PATTERN)
+                    && isIdTypeVariable(variableName, classOrInterfaceBodyNode
+                            .getDescendants(variableDeclarationType))) {
+                hardcodedVariables.put(variableName, current.getParent());
+            }
+        }
+    }
+
+    /**
+     * Verifies if the list of declarations contains a variable of id type and
+     * also verifies if its variable name is equals to the variable name given.
+     *
+     * @param variableName a variable name to compare.
+     * @param declarations a list of declarations.
+     * @return a boolean value true if any declarations name from declarations
+     * list is equal to the variable name given.
+     */
+    boolean isIdTypeVariable(String variableName, List<AstNode> declarations) {
+        return declarations.stream().anyMatch((current)
+                -> (current.getFirstChild(TYPE).getTokenOriginalValue().matches("(?i)id")
+                && variableName.equals(current.getFirstDescendant(VARIABLE_DECLARATOR)
+                        .getTokenOriginalValue())));
+    }
 }
--- apex-checks/src/test/java/org/fundacionjala/enforce/sonarqube/apex/checks/HarcodingIdsCheckTest.java
+++ apex-checks/src/test/java/org/fundacionjala/enforce/sonarqube/apex/checks/HarcodingIdsCheckTest.java
@@ -5,34 +5,35 @@
 
 package org.fundacionjala.enforce.sonarqube.apex.checks;
 
-import com.sonar.sslr.api.Grammar;
+import java.io.File;
+
 import org.fundacionjala.enforce.sonarqube.apex.ApexAstScanner;
 import org.junit.Test;
 import org.sonar.squidbridge.api.SourceFile;
 import org.sonar.squidbridge.checks.CheckMessagesVerifier;
 import org.sonar.squidbridge.checks.SquidCheck;
 
-import java.io.File;
+import com.sonar.sslr.api.Grammar;
 
 public class HarcodingIdsCheckTest {
 
-    private static final String FIRST_VARIABLE_ERROR = "The \"recordId\" variable has hard-coded lines.";
-    private static final String SECOND_VARIABLE_ERROR = "The \"secondRecord\" variable has hard-coded lines.";
-    private static final String FIRST_EXPRESSION_ERROR_MESSAGE = "Statement contains hard-coded value '4654134167168118'.";
-    private static final String SECOND_EXPRESSION_ERROR_MESSAGE = "Statement contains hard-coded value '65469451346546135'.";
-    private static final String THIRD_EXPRESSION_ERROR_MESSAGE = "Statement contains hard-coded value '546546455454654AWB'.";
-    private static final String FOURTH_EXPRESSION_ERROR_MESSAGE = "Statement contains hard-coded value '554546546465456'.";
+    private static final String FIRST_VARIABLE_ERROR = "Variable \"firsRecordId\" contains hard-coded id value.";
+    private static final String SECOND_VARIABLE_ERROR = "Variable \"recordId\" contains hard-coded id value.";
+    private static final String THIRD_VARIABLE_ERROR = "Variable \"secondRecord\" contains hard-coded id value.";
+    private static final String FIRST_EXPRESSION_ERROR_MESSAGE = "Statement contains hard-coded value '554654134167168118'.";
+    private static final String SECOND_EXPRESSION_ERROR_MESSAGE = "Statement contains hard-coded value '554546546465456'.";
     
     private SourceFile sourceFile;
     
     @Test
     public void testHardcodedIdsInVariables() {
-        String filePath = "src/test/resources/checks/HarcodedIdsInVariables.cls";
+        String filePath = "src/test/resources/checks/harcodedIdsInVariables.cls";
         scanFilesWithCheck(filePath, new HardcodingIdsCheckInVariables());
 
         CheckMessagesVerifier.verify(sourceFile.getCheckMessages())
                 .next().atLine(3).withMessage(FIRST_VARIABLE_ERROR)
-                .next().atLine(5).withMessage(SECOND_VARIABLE_ERROR)
+                .next().atLine(10).withMessage(SECOND_VARIABLE_ERROR)
+                .next().atLine(12).withMessage(THIRD_VARIABLE_ERROR)
                 .noMore();
     }
     
@@ -43,9 +44,7 @@ public class HarcodingIdsCheckTest {
 
         CheckMessagesVerifier.verify(sourceFile.getCheckMessages())
                 .next().atLine(4).withMessage(FIRST_EXPRESSION_ERROR_MESSAGE)
-                .next().atLine(9).withMessage(THIRD_EXPRESSION_ERROR_MESSAGE)
-                .next().atLine(11).withMessage(SECOND_EXPRESSION_ERROR_MESSAGE)
-                .next().atLine(15).withMessage(FOURTH_EXPRESSION_ERROR_MESSAGE)
+                .next().atLine(15).withMessage(SECOND_EXPRESSION_ERROR_MESSAGE)
                 .noMore();
     }
     
